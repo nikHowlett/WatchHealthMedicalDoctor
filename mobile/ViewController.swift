55b2d9b615446f1eed8266dc2e06eff12206c3fe
MAC-ATL019922 <mac-atl019922@MAC-ATL019922-Workroom.local>
2015-06-25 11:28:35 -0400

    Initial Commit

---
+++ mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobile
+//
+//  Created by MAC-ATL019922 on 6/25/15.
+//  Copyright (c) 2015 UCB+nikhowlett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
